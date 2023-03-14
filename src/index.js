01b262415859a7a498a39eea0fce3d80da541b58
HovNarek <narek-96@list.ru>
2023-03-15 01:52:59 +0400

    Deploy website

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import App from './App'
 import './index.css'
 
 ReactDOM.render(
-  <React.StrictMode>
-      <App />
+  <BrowserRouter>
+    <App />
   </BrowserRouter>,
   document.getElementById('root')
 )
